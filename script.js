750d99ceaa7bf498108aa67140c81005c741c125
priyanshugupta34701-maker <priyanshugupta34701@gmail.com>
2026-03-18 10:52:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,25 @@ function downloadQR(id){
 
 }
 
+document.getElementById("searchInput").addEventListener("keyup", function(){
 
+    const value = this.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        const text = card.innerText.toLowerCase();
+
+        if(text.includes(value)){
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+});
 
 
 
