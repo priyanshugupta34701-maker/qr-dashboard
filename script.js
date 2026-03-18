507e9d911a08259e92cf9695ff806cd73530299b
priyanshugupta34701-maker <priyanshugupta34701@gmail.com>
2026-03-18 10:14:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,10 +19,8 @@ fetch(sheetURL)
   <p>${item.Location}</p>
 
   <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent('https://priyanshugupta34701-maker.github.io/qr-dashboard/tracking.html?wid=' + item.ID)}" alt="QR">
-
-  <a href="https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=${encodeURIComponent('https://priyanshugupta34701-maker.github.io/qr-dashboard/tracking.html?wid=' + item.ID)}" download>
-    Download QR
-  </a>
+  <button onclick="downloadQR('${item.ID}')">Download QR</button>
+  
 `;
 
     container.appendChild(card);
@@ -34,3 +32,37 @@ fetch(sheetURL)
 function openTracking(id){
   window.location.href = `tracking.html?wid=${id}`;
 }
+
+function downloadQR(id){
+
+  const url = `https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=https://priyanshugupta34701-maker.github.io/qr-dashboard/tracking.html?wid=${id}`;
+
+  fetch(url)
+    .then(res => res.blob())
+    .then(blob => {
+
+      const link = document.createElement("a");
+
+      link.href = URL.createObjectURL(blob);
+
+      link.download = `${id}-QR.png`;
+
+      link.click();
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
